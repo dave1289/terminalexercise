2a367fc33e7e7e9a14b8a0e5ced33506fa2acb5d
David McElhaney <104801768+dave1289@users.noreply.github.com>
2022-07-20 21:53:55 -0400

    Add files via upload

---
+++ term.js
@@ -0,0 +1,21 @@
+mkdir first
+cd first 
+touch person.txt 
+mv person.txt another.txt
+cp another.txt copy.txt
+rm copy.txt 
+cp -r first second
+rm -r second
+
+manual, arrow keys, q 
+
+ignore all 
+ctrl + arrow keys 
+ctrl + e
+ctrl + a
+ctrl + w 
+shell runs on terminal 
+absolute path is the file locations entire address
+relative path is the files location in relation to the parent
+flags alter commands.  cp -r (copy folder) ls -a (shows hidden files as well), ls -c (change only access time)
+remove directories and their contents / force removal
